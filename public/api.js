68966e5e357f5e5e07b948e9a6b73dce1ee2ce6b
andrewwestgard <westgaar9629@uwec.edu>
2020-08-03 14:07:19 -0500

    initialized repo with starter code

---
+++ public/api.js
@@ -0,0 +1,46 @@
+const API = {
+  async getLastWorkout() {
+    let res;
+    try {
+      //get route that fetches workouts
+      res = await fetch("/api/workouts");
+    } catch (err) {
+      console.log(err)
+    }
+    const json = await res.json();
+
+    return json[json.length - 1];
+  },
+  async addExercise(data) {
+    const id = location.search.split("=")[1];
+    //get route that updated individual workout by id
+    // /api/workout/:id
+    const res = await fetch("/api/workouts/" + id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+  async createWorkout(data = {}) {
+    const res = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+
+  async getWorkoutsInRange() {
+    const res = await fetch(`/api/workouts/range`);
+    const json = await res.json();
+
+    return json;
+  },
+};
