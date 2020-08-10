4d22bd8e9e11f05e0e74e03012aee36e80b6fbe7
andrewwestgard <westgaar9629@uwec.edu>
2020-08-09 20:52:37 -0500

    updated html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,14 +4,14 @@ const router = express.Router()
 
 module.exports = function(app) {
     app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname, './public/index.html'))
+        res.sendFile(path.join(__dirname, '../public/index.html'))
     });
 
     app.get('/stats', function(req, res){
-        res.sendFile(path.join(__dirname, './public/stats.html'))
+        res.sendFile(path.join(__dirname, '../public/stats.html'))
     });
 
     app.get('/exercise', function(req, res){
-        res.sendFile(path.join(__dirname, './public/exercise.html'))
+        res.sendFile(path.join(__dirname, '../public/exercise.html'))
     });
 }
